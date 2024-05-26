c1d28ca1e68be32a9ec543b93c9c60840dc4d030
Shutcing <maksim19111@gmail.com>
2024-05-27 00:28:45 +0500

    Create Arrows.cs

---
+++ Train Runner/Assets/Scripts/Arrows.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Arrows : MonoBehaviour
+{
+    public List<string> skins = new List<string> { "Ryan", "Ken" };
+
+
+    public void OnButtonClick(string direction)
+    {
+        if (direction == "left" && move.currentSkin == 1)
+        {
+            move.currentSkin = 0;
+            move.skin = skins[move.currentSkin];
+        }
+        else if (direction == "right" && move.currentSkin == 0)
+        {
+            move.currentSkin = 1;
+            move.skin = skins[move.currentSkin];
+        }
+    }
+
+
+    void Update()
+    {
+        string objectName = gameObject.name;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Math.Abs(transform.position.x - mousePosition.x) < 0.5 && Math.Abs(transform.position.y - mousePosition.y) < 0.5 && Input.GetMouseButtonDown(0) && objectName == "ArrowLeft")
+        {
+            OnButtonClick("left");
+        }
+        else if (Math.Abs(transform.position.x - mousePosition.x) < 0.5 && Math.Abs(transform.position.y - mousePosition.y) < 0.5 && Input.GetMouseButtonDown(0) && objectName == "ArrowRight")
+        {
+            OnButtonClick("right");
+        }
+    }
+}
+
