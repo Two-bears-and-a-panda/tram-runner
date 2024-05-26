3ac9bff30c1963c55e02860e81e85a22a46cbbc5
Shutcing <maksim19111@gmail.com>
2024-05-27 00:29:19 +0500

    Update Leg_script.cs

--- Train Runner/Assets/Scripts/Leg_script.cs
+++ Train Runner/Assets/Scripts/Leg_script.cs
@@ -6,52 +6,46 @@ using System;
 public class Leg_script : MonoBehaviour
 {
     private static float speed = 3f;
-    private static bool flag = false;
     private Renderer visual;
-
     private int tact = 0;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         visual = GetComponent<Renderer>();
-        visual.enabled = !visual.enabled;
-        StartCoroutine(ExampleCoroutine(5));
+        visual.enabled = false;
     }
 
     IEnumerator ExampleCoroutine(int seconds)
     {
-        //yield on a new YieldInstruction that waits for 5 seconds.
         yield return new WaitForSeconds(seconds);
-        flag = true;
         visual.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (flag)
+        if (GameManager.RyanMove)
         {
+            visual.enabled = true;
+
             tact+=4;
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             string objectName = gameObject.name;
 
-
+            mousePosition.y -= 1.6f;
             if (objectName == "Leg1")
             {
-                mousePosition.y -= 1.6f;
                 mousePosition.x -= 0.5f;
             }
             else
             {
-                mousePosition.y -= 1.6f;
                 mousePosition.x += 0.5f;
             }
 
             transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
             if (objectName == "Leg1")
             {
-
                 if (transform.position.x > mousePosition.x)
                 {
                     if (tact % 4 == 0)
