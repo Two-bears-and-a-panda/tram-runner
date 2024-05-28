d82507254dac064b718df5753dcb2ddb3ed7ece1
Shutcing <maksim19111@gmail.com>
2024-05-28 20:29:54 +0500

    Update Hand_script.cs

--- Train Runner/Assets/Scripts/Hand_script.cs
+++ Train Runner/Assets/Scripts/Hand_script.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Hand_script : MonoBehaviour
 {
     private static float speed = 3f;
     private Renderer visual;
+    private bool left = false;
+    private bool right = false;
+    private int angleLeft = 0;
+    private int angleRight = 0;
 
 
     void Start()
@@ -13,6 +18,13 @@ public class Hand_script : MonoBehaviour
         visual = GetComponent<Renderer>();
         visual.enabled = false;
     }
+
+    IEnumerator ExampleCoroutine(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        visual.enabled = true;
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -26,19 +38,49 @@ public class Hand_script : MonoBehaviour
             if (objectName == "Hand1")
             {
                 mousePosition.y -= 1f;
-                mousePosition.x -= 0.8f;
+                mousePosition.x -= 1.3f;
             }
             else
             {
                 mousePosition.y -= 1f;
-                mousePosition.x += 0.8f;
+                mousePosition.x += 1.3f;
             }
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && !batScript.TakeIt)
             {
-                for (int i = 0; i < 100; i++)
-                    transform.Rotate(0, 0, -900 * Time.deltaTime, Space.Self);
+                if (move.moveDirection == "left" && objectName == "Hand1")
+                {
+                    left = true;
+                    right = false;
+                }
+                else if (move.moveDirection == "right" && objectName == "Hand2")
+                {
+                    right = true;
+                    left = false;
+                }
             }
             transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
+
+
+            if (left && objectName == "Hand1")
+            {
+                transform.Rotate(0, 3, 0, Space.Self);
+                angleLeft += 3;
+                if (angleLeft == 360)
+                {
+                    left = false;
+                    angleLeft = 0;
+                }
+            }
+            else if (right && objectName == "Hand2")
+            {
+                transform.Rotate(0, 3, 0, Space.Self);
+                angleRight += 3;
+                if (angleRight == 360)
+                {
+                    right = false;
+                    angleRight = 0;
+                }
+            }
         }
     }
 }
